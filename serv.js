dc764aa98b671b7d2db423e40880ed5eb10e9537
MelissaGimson <MelissaGimson92@hotmail.com>
2024-04-14 15:47:09 +0100

    init

---
+++ serv.js
@@ -0,0 +1,12 @@
+const sdk = require("iot-lorawan")
+
+let device = new iot-lorawan.Device({
+        clientId: opts.clientId || "simulator",
+        caPath: directory + "/ca.pem",
+        keyPath: directory + "/key.pem",
+        certPath: directory + "/cert.pem",
+        meta: {
+            model: "simulator",
+            desc: "Simulated IOT device using the senzflow(©) sdk"
+        }
+    });
